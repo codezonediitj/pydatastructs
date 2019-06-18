3785428cda87d288d53b0a1d7e604dbb5494b820
czgdp1807 <singh.23@iitj.ac.in>
2019-06-18 17:32:51 +0530

    completed array

--- pydatastructs/linear_data_structures/tests/test_arrays.py
+++ pydatastructs/linear_data_structures/tests/test_arrays.py
@@ -1,15 +1,16 @@
 from pydatastructs.linear_data_structures import OneDimensionalArray
 from pydatastructs.utils.raises_util import raises
 
+
 def test_OneDimensionalArray():
     ODA = OneDimensionalArray
-    assert ODA(5, [1, 2, 3, 4, 5], init=6)
-    assert ODA((1, 2, 3, 4, 5), 5)
-    assert ODA(5)
-    assert ODA([1, 2, 3])
+    assert ODA(int, 5, [1, 2, 3, 4, 5], init=6)
+    assert ODA(int, (1, 2, 3, 4, 5), 5)
+    assert ODA(int, 5)
+    assert ODA(int, [1, 2, 3])
     raises(ValueError, lambda: ODA())
-    raises(ValueError, lambda: ODA(1, 2, 3))
-    raises(TypeError, lambda: ODA(5.0, set([1, 2, 3])))
-    raises(TypeError, lambda: ODA(5.0))
-    raises(TypeError, lambda: ODA(set([1, 2, 3])))
-    raises(ValueError, lambda: ODA(3, [1]))
+    raises(ValueError, lambda: ODA(int, 1, 2, 3))
+    raises(TypeError, lambda: ODA(int, 5.0, set([1, 2, 3])))
+    raises(TypeError, lambda: ODA(int, 5.0))
+    raises(TypeError, lambda: ODA(int, set([1, 2, 3])))
+    raises(ValueError, lambda: ODA(int, 3, [1]))
