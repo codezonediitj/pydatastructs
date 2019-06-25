ca19ab53aa03ddf2dd3cc2cb08793f0ecc803f94
czgdp1807 <singh.23@iitj.ac.in>
2019-06-25 12:35:23 +0530

    created misc_util

--- pydatastructs/trees/space_partitioning_trees.py
+++ pydatastructs/trees/space_partitioning_trees.py
@@ -1,4 +1,4 @@
-from pydatastructs.trees.binary_trees import Node
+from pydatastructs.utils import Node
 from collections import deque as Queue
 from pydatastructs.linear_data_structures.arrays import _check_type
 
