b86aa9925510616e008452c14d68c9ded060b996
czgdp1807 <singh.23@iitj.ac.in>
2019-07-04 15:37:53 +0530

    added new version of node

--- pydatastructs/utils/misc_util.py
+++ pydatastructs/utils/misc_util.py
@@ -1,7 +1,8 @@
 from __future__ import print_function, division
 
 __all__ = [
-    'Node'
+    'Node',
+    'NodeV2'
 ]
 
 _check_type = lambda a, t: isinstance(a, t)
@@ -38,3 +39,22 @@ class Node(object):
         Used for printing.
         """
         return str((self.left, self.key, self.data, self.right))
+
+class NodeV2(Node):
+    """
+    Extends `Node` by storing, height and parent.
+
+    Parameters
+    ==========
+
+    height: int
+        Height of the node,
+        given as max(height of left child, height of right child).
+    parent: int
+        The index of the parent node.
+    """
+
+    def __new__(cls, key, data):
+        obj = super(cls, NodeV2).__new__(cls, key, data)
+        obj.parent, obj.height = None, 0
+        return obj
