b61bc2865aa6ebf3615292efce5b82bd7ef020d0
TarunTomar122 <tomartarun2001@gmail.com>
2019-12-20 15:16:29 +0530

    Fixed test issues

--- pydatastructs/miscellaneous_data_structures/tests/test_stack.py
+++ pydatastructs/miscellaneous_data_structures/tests/test_stack.py
@@ -10,16 +10,16 @@ def test_Stack():
     s.push(3)
     assert s.top == 3
     assert str(s) == '[1, 2, 3]'
-    raises(ValueError, lambda: s.push(4))
+    assert raises(ValueError, lambda: s.push(4))
     assert s.pop() == 3
     assert s.pop() == 2
     assert s.pop() == 1
     assert s.top == 0
-    raises(ValueError, lambda: s.pop())
-    raises(ValueError, lambda: Stack())
-    raises(TypeError, lambda: Stack(maxsize=8, top=3.5))
-    raises(ValueError, lambda: Stack(maxsize=5, top=0, items=[1, 2, 3]))
-    raises(ValueError, lambda: Stack(maxsize=5, top=0,
+    assert raises(ValueError, lambda: s.pop())
+    assert raises(ValueError, lambda: Stack())
+    assert raises(TypeError, lambda: Stack(maxsize=8, top=3.5))
+    assert raises(ValueError, lambda: Stack(maxsize=5, top=0, items=[1, 2, 3]))
+    assert raises(ValueError, lambda: Stack(maxsize=5, top=0,
                         items=OneDimensionalArray(int, 6)))
-    raises(NotImplementedError, lambda: Stack(implementation='',
+    assert raises(NotImplementedError, lambda: Stack(implementation='',
                                     maxsize=5, top=0))
