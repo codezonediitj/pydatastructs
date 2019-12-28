2442914155635dbebe6f66b5fa7bbd74638b77a2
Utkarsh Mishra <35002267+utkarsh0702@users.noreply.github.com>
2019-12-29 01:09:56 +0530

    Update __init__.py

--- pydatastructs/miscellaneous_data_structures/__init__.py
+++ pydatastructs/miscellaneous_data_structures/__init__.py
@@ -2,9 +2,14 @@ __all__ = []
 
 from . import (
     stack,
+    queue,
 )
 
 from .stack import (
     Stack,
 )
+from .queue import (
+    Queue,
+)
 __all__.extend(stack.__all__)
+__all__.extend(queue.__all__)
