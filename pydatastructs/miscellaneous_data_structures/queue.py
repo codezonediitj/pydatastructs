fa2ee9ac1543597cfbbeb642e1bd2b564d14e3f1
Utkarsh Mishra <35002267+utkarsh0702@users.noreply.github.com>
2020-01-01 22:14:05 +0530

    Updated the queue.py acc. to Dynamic 1d

--- pydatastructs/miscellaneous_data_structures/queue.py
+++ pydatastructs/miscellaneous_data_structures/queue.py
@@ -6,13 +6,11 @@ __all__ = [
 
 _check_type = lambda a, t: isinstance(a, t)
 NoneType = type(None)
-rear= -1
 
 class Queue(objects):
     def __new__(clas, implementation='array', **kwargs):
         if implementation == 'array':
             return ArrayQueue(
-                kwargs.get('front', -1),
                 kwargs.get('item', None),
                 kwargs.get('dtype', int))
         raise NotImplementedError("%s hasn't been implemented yet."%(implementation))
@@ -23,25 +21,29 @@ class Queue(objects):
    def popleft(self, **kwargs):
         raise NotImplementedError("This is an abstract method.")
         
+   def __len__(self, **kwargs):
+        raise NotImplementedError("This is an abstract method.")
+        
         
 class ArrayQueue(Queue):
-    __slots__ = ['front','item','dtype','count']
-    def __new__(clas, front = -1, item = None, dtype = int):
-        if not _check_type(front, int):
-            raise TypeError("front is not of type int.")
+    __slots__ = ['item','dtype']
+    front= -1
+    count= 0
+    def __new__(clas, item = None, dtype = int):
         if item is None:
             item= DynamicOneDimensionalArray(dtype, 0)
         else:
             item= DynamicOneDimensionalArray(dtype, item)
             
         obj = object.__new__(clas)
-        obj.front, obj.item, obj.dtype = front, item, items._dtype
+        obj.item, obj.dtype = item, items._dtype
         return obj
         
     def append(self, x):
             if self.front == -1:
                 self.front=0
             self.item.append(x)
+            self.count+=1
 
     def popleft(self):
         if (self.front == -1):
@@ -50,6 +52,12 @@ class ArrayQueue(Queue):
         self.item.delete(self.front)
         self.front += 1
         return r
+    
+    def __len__(self):
+        if (self.front == -1):
+            return 0
+        else:
+            return (self.count- self.front)
         
     def __str__(self):
         """
