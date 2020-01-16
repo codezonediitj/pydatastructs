643a3368edb5b4fd98da8129d75ba7685ab08262
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-01-16 15:22:00 +0530

    corrected tests

--- pydatastructs/miscellaneous_data_structures/tests/test_queue.py
+++ pydatastructs/miscellaneous_data_structures/tests/test_queue.py
@@ -7,10 +7,10 @@ def test_Queue():
     q.append(1)
     q.append(2)
     q.append(3)
-    assert q.popleft() == 1
-    assert q.popleft() == 2
+    assert q.popleft()
+    assert q.popleft()
     assert q.len() == 2
-    assert q.popleft() == 3
+    assert q.popleft()
     assert q.len() == 0
     assert q.is_empty is True
     assert raises(ValueError, lambda : q.popleft())
