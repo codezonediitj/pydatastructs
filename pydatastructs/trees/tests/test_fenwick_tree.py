ef04fafdd302e2c69b43943cd956a7a5beb4d018
aravind_k <aravind@jungroo.com>
2020-02-21 11:17:10 +0530

    Added Fenwick Trees

--- pydatastructs/trees/tests/test_fenwick_tree.py
+++ pydatastructs/trees/tests/test_fenwick_tree.py
@@ -6,6 +6,8 @@ def test_FenwickTree():
     t = FT([1,2,3,4,5,6,7,8,9,10])
     assert t.get_sum(0,2) == 6
     assert t.get_sum(0,4) == 15
+    assert t.get_sum(0,9) == 55
     t.update(0,100)
     assert t.get_sum(0,2) == 105
     assert t.get_sum(0,4) == 114
+    assert t.get_sum(0,9) == 154
