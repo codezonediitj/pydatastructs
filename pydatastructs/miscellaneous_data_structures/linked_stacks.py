24aff3463aff61e55cec1a74bec4d23717a9dc62
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-03-03 13:55:47 -0500

    Updated Linked_Stacks

--- pydatastructs/miscellaneous_data_structures/linked_stacks.py
+++ pydatastructs/miscellaneous_data_structures/linked_stacks.py
@@ -3,19 +3,15 @@ from pydatastructs import SinglyLinkedList
 class Linked_Stacks:
     
     # A class to implement Stacks Built over Singly Linked Lists
-
-
+    
     def __init__(self):
         self.sll = SinglyLinkedList()
         self.top = self.sll.head
     
-
     def push(self,data):
-        
         self.sll.append(data)
         if self.top is None:
             self.top = self.sll.head
-        
         return self
 
     def pop(self):
@@ -26,12 +22,3 @@ class Linked_Stacks:
     
     def peek(self):
         return self.top.data
-        
-new_stack = Linked_Stacks()
-new_stack.push(6)
-new_stack.push(7)
-new_stack.push(13)
-data = new_stack.pop()
-output = new_stack.peek()
-
-
