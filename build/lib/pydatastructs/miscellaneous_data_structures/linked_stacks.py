4c06b1cd15db4cc96d5d53b1f1fc1750381b37e7
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-03-04 08:47:33 -0500

    Updates

--- build/lib/pydatastructs/miscellaneous_data_structures/linked_stacks.py
+++ build/lib/pydatastructs/miscellaneous_data_structures/linked_stacks.py
@@ -1,1 +1,25 @@
-from pydatastructs.linear_data_structures.linked_lists import DoublyLinkedList
+from pydatastructs import SinglyLinkedList
+
+__all__ = ["Linked_Stacks"]
+class Linked_Stacks:
+    
+    # A class to implement Stacks Built over Singly Linked Lists
+    
+    def __init__(self):
+        self.sll = SinglyLinkedList()
+        self.top = self.sll.head
+    
+    def push(self,data):
+        self.sll.append(data)
+        if self.top is None:
+            self.top = self.sll.head
+        return self
+
+    def pop(self):
+        if self.top is not None:
+            self.data = self.top.data
+            self.top = self.top.next
+        return self.data
+    
+    def peek(self):
+        return self.top.data
