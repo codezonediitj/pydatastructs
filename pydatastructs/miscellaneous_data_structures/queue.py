d2ea7957f2f072daadd99cd434e331b7cf73447b
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-03-10 13:13:20 -0400

    Updated Priority Queue

--- pydatastructs/miscellaneous_data_structures/queue.py
+++ pydatastructs/miscellaneous_data_structures/queue.py
@@ -1,4 +1,4 @@
-from pydatastructs.linear_data_structures import DynamicOneDimensionalArray
+from pydatastructs.linear_data_structures import DynamicOneDimensionalArray , SinglyLinkedList
 from pydatastructs.utils.misc_util import NoneType
 from copy import deepcopy as dc
 
@@ -6,31 +6,22 @@ __all__ = [
     'Queue'
 ]
 
-
 class Queue(object):
-    """Representation of queue data structure.
-
+    """
+    Representation of queue data structure.
     Parameters
     ==========
-
     implementation : str
         Implementation to be used for queue.
         By default, 'array'
-        Currently only supports 'array'
-        implementation.
     items : list/tuple
         Optional, by default, None
         The inital items in the queue.
-        For array implementation.
     dtype : A valid python type
         Optional, by default NoneType if item
-        is None, otherwise takes the data
-        type of DynamicOneDimensionalArray
-        For array implementation.
-
+        is None.
     Examples
     ========
-
     >>> from pydatastructs import Queue
     >>> q = Queue()
     >>> q.append(1)
@@ -40,10 +31,8 @@ class Queue(object):
     1
     >>> len(q)
     2
-
     References
     ==========
-
     .. [1] https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
     """
 
@@ -52,12 +41,15 @@ class Queue(object):
             return ArrayQueue(
                 kwargs.get('items', None),
                 kwargs.get('dtype', int))
-        elif implementation=="pq":
+        elif implementation == 'linkedlist':
+            return LinkedListQueue(
+                kwargs.get('items', None),
+                kwargs.get('dtype', NoneType)
+            )
+        elif implementation=="priority queue":
             return PriorityQueue()
         raise NotImplementedError(
                 "%s hasn't been implemented yet."%(implementation))
-
-
 
     def append(self, *args, **kwargs):
         raise NotImplementedError(
@@ -69,7 +61,9 @@ class Queue(object):
 
     @property
     def is_empty(self):
-        return None
+        raise NotImplementedError(
+            "This is an abstract method.")
+
 
 class ArrayQueue(Queue):
 
@@ -128,13 +122,76 @@ class ArrayQueue(Queue):
             _data.append(self.items._data[i])
         return str(_data)
 
+
+class LinkedListQueue(Queue):
+
+    __slots__ = ['front', 'rear', 'size', '_dtype']
+
+    def __new__(cls, items=None, dtype=NoneType):
+        obj = object.__new__(cls)
+        obj.queue = SinglyLinkedList()
+        obj._dtype = dtype
+        if items is None:
+            pass
+        elif type(items) in (list, tuple):
+            if len(items) != 0 and dtype is NoneType:
+                obj._dtype = type(items[0])
+            for x in items:
+                if type(x) == obj._dtype:
+                    obj.queue.append(x)
+                else:
+                    raise TypeError("Expected %s but got %s"%(obj._dtype, type(x)))
+        else:
+            raise TypeError("Expected type: list/tuple")
+        obj.front = obj.queue.head
+        obj.rear = obj.queue.tail
+        obj.size = obj.queue.size
+        return obj
+
+    def append(self, x):
+        if self._dtype is NoneType:
+            self._dtype = type(x)
+        elif type(x) is not self._dtype:
+            raise TypeError("Expected %s but got %s"%(self._dtype, type(x)))
+        self.size += 1
+        self.queue.append(x)
+        if self.front is None:
+            self.front = self.queue.head
+        self.rear = self.queue.tail
+
+    def popleft(self):
+        if self.is_empty:
+            raise ValueError("Queue is empty.")
+        self.size -= 1
+        return_value = self.queue.pop_left()
+        self.front = self.queue.head
+        self.rear = self.queue.tail
+        return return_value
+
+    @property
+    def is_empty(self):
+        return self.size == 0
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return str(self.queue)
+
 class PriorityQueue(Queue):
 
     '''
     PARAMETERS
     ==========
     insert : method()
-    Used to insert data into the queue
+     - Used to insert data into the queue
+
+    deleteHighestPriority : method()
+     - deletes the element with highest priority in the queue
+     - priority can be min or max
+
+    getHighestPriority : method()
+     - Prints element with Highest Priority in the queue
     '''
     def __new__(cls):
         obj = object.__new__(cls)
