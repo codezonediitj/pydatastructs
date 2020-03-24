3fe551aa8e9d826723ec85ae81eb2027b8828543
ramyaperumandla24 <57004716+ramyaperumandla24@users.noreply.github.com>
2020-03-24 07:53:19 +0530

    Update algorithms.py

--- pydatastructs/graphs/algorithms.py
+++ pydatastructs/graphs/algorithms.py
@@ -220,6 +220,22 @@ def _minimum_spanning_tree_kruskal_adjacency_list(graph):
 
 _minimum_spanning_tree_kruskal_adjacency_matrix = \
     _minimum_spanning_tree_kruskal_adjacency_list
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+graph = {'0': set(['1', '2']),
+         '1': set(['0', '3', '4']),
+         '2': set(['0']),
+         '3': set(['1']),
+         '4': set(['2', '3'])}
+
+dfs(graph, '0')
 
 def minimum_spanning_tree(graph, algorithm):
     """
