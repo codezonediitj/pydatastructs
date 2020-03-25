7724e2951aafd0fd3393b7476fddf8462c0cb59b
Harsheet-saxena <harshsaxena1999@gmail.com>
2020-03-25 22:47:51 +0530

    common test added

--- pydatastructs/linear_data_structures/tests/test_algorithms.py
+++ pydatastructs/linear_data_structures/tests/test_algorithms.py
@@ -3,8 +3,7 @@ from pydatastructs import (
     OneDimensionalArray, brick_sort)
 import random
 
-def test_merge_sort_parallel():
-
+def _test_common_sort(sort, *args, **kwargs):
     random.seed(1000)
 
     n = random.randint(10, 20)
@@ -19,9 +18,9 @@ def test_merge_sort_parallel():
                     None, None, None, None, None,
                     None, None, None, None, None,
                     None, None, None, None]
-    merge_sort_parallel(arr, 5, start=2, end=10)
+    sort(arr, *args, **kwargs, start=2, end=10)
     assert arr._data == expected_arr
-    merge_sort_parallel(arr, 5)
+    sort(arr, *args, **kwargs)
     expected_arr = [102, 134, 228, 247, 362, 373, 448,
                     480, 548, 686, 688, 696, 779,
                     None, None, None, None, None, None,
@@ -37,28 +36,11 @@ def test_merge_sort_parallel():
     expected_arr = [42, 695, 147, 500, 768,
                     998, 473, 732, 728, 426,
                     709, 910]
-    merge_sort_parallel(arr, 5, start=2, end=5)
+    sort(arr, *args, **kwargs, start=2, end=5)
     assert arr._data == expected_arr
 
+def test_merge_sort_parallel():
+    _test_common_sort(merge_sort_parallel, num_threads=5)
+
 def test_brick_sort():
-
-    random.seed(1000)
-    n = 10
-    arr = OneDimensionalArray(int, [random.randint(1,1000) for _ in range(n)])
-    b = sorted(arr)
-    brick_sort(arr)
-    assert b == list(arr)
-
-    arr = OneDimensionalArray(int, [random.randint(1,1000) for _ in range(n)])
-    brick_sort(arr, comp=lambda u, v: u>v)
-    b = list(reversed(sorted(arr)))
-    assert b == list(arr)
-
-    arr = DynamicOneDimensionalArray(int, 0)
-    for i in range(n):
-        arr.append(random.randint(1,1000))
-
-    x = [arr[i] for i in range(arr._last_pos_filled+1)]
-    b = sorted(x)
-    brick_sort(arr)
-    assert b == [arr[i] for i in range(arr._last_pos_filled+1)]
+    _test_common_sort(brick_sort)
