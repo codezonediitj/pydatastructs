b0542cc63cf9b79c0a446ed2f61a4331e51168ef
JeanPierreMR <jpmejicanos@ufm.edu>
2020-04-15 16:51:32 -0600

    Update trie_structure.py

--- pydatastructs/trees/trie_structure.py
+++ pydatastructs/trees/trie_structure.py
@@ -1,12 +1,18 @@
-class TrieNode:
+# Init node class
+class Node:
 
     def __init__(self):
         self.children = [None] * 26  # 26 because we are taking the alphabet_size
 
-        # isEndOfWord is True if node represent the end of the word 
+        # isEndOfWord is True if node represent the end of the word
         self.isEndOfWord = False
 
+        self.parent = None
 
+        self.ch = ' '
+
+
+# Init class trie
 class Trie:
     """
             Represents a trie data structure.
@@ -43,19 +49,18 @@ class Trie:
         self.root = self.getNode()
 
     def getNode(self):
+        return Node()  # Returns new trie node (initialized to NULLs)
 
-        return TrieNode()  # Returns new trie node (initialized to NULLs)
-
-    def _charToIndex(self, ch):
+    def toIndex(self, ch):
         '''
         Notes to know about char to Index func:
-        Converts key current character into index 
-        Using only 'a' through 'z' and lower case 
+        Converts key current character into index
+        Using only 'a' through 'z' and lower case
         Ord() definition: Given a string of length one, return an integer representing the Unicode code point of the character when the argument is a unicode object, or the value of the byte when the argument is an 8-bit string.
         '''
         return ord(ch) - ord('a')
 
-    def insert(self, key):
+    def insertKey(self, key):
         '''
         Notes to know about insertion in trie structure:
         Every character of input key is inserted as an individual trie node.
@@ -65,47 +70,47 @@ class Trie:
         Insert and search costs O(k) where k is length of key.
         The memory requirements of trie is O(alphabet_size*k*N) where N is number of keys in trie.
 
-          
-        If not present, inserts key into trie 
-        If the key is prefix of trie node, just marks leaf node 
+
+        If not present, inserts key into trie
+        If the key is prefix of trie node, just marks leaf node
         '''
         pC = self.root
         length = len(key)
         for level in range(length):
-            index = self._charToIndex(key[level])  # charToIndex function above
+            idx = self.toIndex(key[level])  # charToIndex function above
 
             # if character not present:
-            if not pC.children[index]:
-                pC.children[index] = self.getNode()
-            pC = pC.children[index]
+            if not pC.children[idx]:
+                pC.children[idx] = self.getNode()
+            pC = pC.children[idx]
 
             # marking last node as leaf:
         pC.isEndOfWord = True  # isEndOfWord is True if node represent the end of the word
 
-    def search(self, key):
+    def searchKey(self, key):
         '''
         Notes to know about searching in trie structure:
         While searching we only compare the characters and move down.
         Search key in the trie .
-        Returning true if key presents in trie, else false. 
+        Returning true if key presents in trie, else false.
         The search can end because of end of a string, if the value field of last node is non-zero then the key exists in trie.
         '''
         pC = self.root
         length = len(key)
         for level in range(length):
-            index = self._charToIndex(key[level])  # charToIndex function above
-            if not pC.children[index]:
+            idx2 = self.toIndex(key[level])  # charToIndex function above
+            if not pC.children[idx2]:
                 return False
-            pC = pC.children[index]
+            pC = pC.children[idx2]
 
         return pC != None and pC.isEndOfWord  # and is a boolean expression  | isEndofWord: True if node represent the end of the word
 
     def delete(self, key):
         pC = self.root
 
-        if self.search(key):
+        if self.searchKey(key):
             for c in key:
-                index = self._charToIndex(c)
-                pC = pC.children[index]
+                idx3 = self.toIndex(c)
+                pC = pC.children[idx3]
 
             pC.isEndOfWord = False
