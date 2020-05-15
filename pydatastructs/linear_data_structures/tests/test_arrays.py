5f5e3869f25773c5afb64dd8210531f2e7c8237b
JeanPierreMR <54464816+JeanPierreMR@users.noreply.github.com>
2020-05-15 09:11:03 -0600

    Multi-dimensional array added (#256)

--- pydatastructs/linear_data_structures/tests/test_arrays.py
+++ pydatastructs/linear_data_structures/tests/test_arrays.py
@@ -1,5 +1,5 @@
 from pydatastructs.linear_data_structures import (
-    OneDimensionalArray, DynamicOneDimensionalArray)
+    OneDimensionalArray, DynamicOneDimensionalArray, MultiDimensionalArray)
 from pydatastructs.utils.raises_util import raises
 
 
@@ -20,6 +20,39 @@ def test_OneDimensionalArray():
     assert raises(TypeError, lambda: ODA(int, 5.0))
     assert raises(TypeError, lambda: ODA(int, set([1, 2, 3])))
     assert raises(ValueError, lambda: ODA(int, 3, [1]))
+
+
+def test_MultiDimensionalArray():
+    assert raises(ValueError, lambda: MultiDimensionalArray(int, 2, -1, 3))
+    assert MultiDimensionalArray(int, 10).shape == (10,)
+    array = MultiDimensionalArray(int, 5, 9, 3, 8)
+    assert array.shape == (5, 9, 3, 8)
+    array.fill(5)
+    array[1, 3, 2, 5] = 2.0
+    assert array
+    assert array[1, 3, 2, 5] == 2.0
+    assert array[1, 3, 0, 5] == 5
+    assert array[1, 2, 2, 5] == 5
+    assert array[2, 3, 2, 5] == 5
+    assert raises(IndexError, lambda: array[5])
+    assert raises(IndexError, lambda: array[4, 10])
+    assert raises(IndexError, lambda: array[-1])
+    assert raises(IndexError, lambda: array[2, 3, 2, 8])
+    assert raises(ValueError, lambda: MultiDimensionalArray())
+    assert raises(ValueError, lambda: MultiDimensionalArray(int))
+    assert raises(TypeError, lambda: MultiDimensionalArray(int, 5, 6, ""))
+    array = MultiDimensionalArray(int, 3, 2, 2)
+    array.fill(1)
+    array[0, 0, 0] = 0
+    array[0, 0, 1] = 0
+    array[1, 0, 0] = 0
+    array[2, 1, 1] = 0
+    assert str(array) == '[0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0]'
+    array = MultiDimensionalArray(int, 4)
+    assert array.shape == (4,)
+    array.fill(5)
+    array[3] = 3
+    assert array[3] == 3
 
 def test_DynamicOneDimensionalArray():
     DODA = DynamicOneDimensionalArray
