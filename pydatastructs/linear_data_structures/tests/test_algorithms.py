6299dfa494318e34b140fff7d32ae6dc0e187e26
pravalikavis <pravalikavis@gmail.com>
2020-05-27 23:59:45 +0530

    test cases for optimal grouping

--- pydatastructs/linear_data_structures/tests/test_algorithms.py
+++ pydatastructs/linear_data_structures/tests/test_algorithms.py
@@ -1,9 +1,10 @@
 from pydatastructs import (
     merge_sort_parallel, DynamicOneDimensionalArray,
     OneDimensionalArray, brick_sort, brick_sort_parallel,
-    heapsort, matrix_multiply_parallel)
+    heapsort, matrix_multiply_parallel, optimal_grouping)
 from pydatastructs.utils.raises_util import raises
 import random
+
 
 def _test_common_sort(sort, *args, **kwargs):
     random.seed(1000)
@@ -41,25 +42,32 @@ def _test_common_sort(sort, *args, **kwargs):
     sort(arr, *args, **kwargs, start=2, end=5)
     assert arr._data == expected_arr
 
+
 def test_merge_sort_parallel():
     _test_common_sort(merge_sort_parallel, num_threads=5)
+
 
 def test_brick_sort():
     _test_common_sort(brick_sort)
 
+
 def test_brick_sort_parallel():
     _test_common_sort(brick_sort_parallel, num_threads=3)
 
+
 def test_heapsort():
     _test_common_sort(heapsort)
+
 
 def test_matrix_multiply_parallel():
     ODA = OneDimensionalArray
 
     expected_result = [[3, 3, 3], [1, 2, 1], [2, 2, 2]]
 
-    I = ODA(ODA, [ODA(int, [1, 1, 0]), ODA(int, [0, 1, 0]), ODA(int, [0, 0, 1])])
-    J = ODA(ODA, [ODA(int, [2, 1, 2]), ODA(int, [1, 2, 1]), ODA(int, [2, 2, 2])])
+    I = ODA(ODA, [ODA(int, [1, 1, 0]), ODA(
+        int, [0, 1, 0]), ODA(int, [0, 0, 1])])
+    J = ODA(ODA, [ODA(int, [2, 1, 2]), ODA(
+        int, [1, 2, 1]), ODA(int, [2, 2, 2])])
     output = matrix_multiply_parallel(I, J, num_threads=5)
     assert expected_result == output
 
@@ -70,9 +78,38 @@ def test_matrix_multiply_parallel():
 
     I = [[1, 1, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1]]
     J = [[2, 1, 2], [1, 2, 1], [2, 2, 2]]
-    assert raises(ValueError, lambda: matrix_multiply_parallel(I, J, num_threads=5))
+    assert raises(ValueError, lambda: matrix_multiply_parallel(
+        I, J, num_threads=5))
 
     I = [[1, 1, 0], [0, 1, 0], [0, 0, 1]]
     J = [[2, 1, 2], [1, 2, 1], [2, 2, 2]]
     output = matrix_multiply_parallel(I, J, num_threads=1)
     assert expected_result == output
+
+
+def test_optimal_grouping():
+    #test case1:
+    def cost(matrix, endIndex, middle):
+
+        if endIndex[0] == endIndex[1]:
+            return 0
+        return matrix[endIndex[0]-1]*matrix[middle]*matrix[endIndex[1]]
+
+    def lookup(position, endIndex, middle):
+        if position is 'before':
+            return [endIndex[0], middle]
+        else:
+            return [middle+1, endIndex[1]]
+    expected_result = [[float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf')],
+                       [float('inf'), 1, 1, 1, 3, 3, 3, float('inf')],
+                       [float('inf'), float('inf'), 2, 2, 3, 3, 3, float('inf')],
+                       [float('inf'), float('inf'), float('inf'), 3, 3, 3, 3, float('inf')],
+                       [float('inf'), float('inf'), float('inf'), float('inf'), 4, 4, 5, float('inf')],
+                       [float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), 5, 5, float('inf')],
+                       [float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), 6, float('inf')],
+                       [float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf')]]
+    assert expected_result == optimal_grouping([30, 35, 15, 5, 10, 20, 25], False, 2, [1, 6], lookup, cost)
+
+    #test case2:
+    expected_result = [[0, float('inf'), float('inf')], [float('inf'), float('inf'), float('inf')], [float('inf'), float('inf'), float('inf')]]
+    assert expected_result == optimal_grouping([], False, 2, [0,0], lookup, cost)
