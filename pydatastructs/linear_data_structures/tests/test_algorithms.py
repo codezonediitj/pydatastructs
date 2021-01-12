1ae0e3dd598426ad7054e582294ea1d6ae7843bb
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2021-01-12 16:17:02 +0530

    Update test_algorithms.py

--- pydatastructs/linear_data_structures/tests/test_algorithms.py
+++ pydatastructs/linear_data_structures/tests/test_algorithms.py
@@ -72,18 +72,7 @@ def test_counting_sort():
     assert counting_sort(arr)._data == expected_arr
 
 def test_cocktail_sort():
-    random.seed(1000)
-
-    n = random.randint(10, 20)
-    arr = DynamicOneDimensionalArray(int, 0)
-    for _ in range(n):
-        arr.append(random.randint(1, 1000))
-    for _ in range(n//3):
-        arr.delete(random.randint(0, n//2))
-
-    expected_arr = [102, 134, 228, 247, 362, 373, 448,
-                    480, 548, 686, 688, 696, 779]
-    assert cocktail_sort(arr)._data == expected_arr
+    _test_common_sort(cocktail_sort)
 
 def test_matrix_multiply_parallel():
     ODA = OneDimensionalArray
