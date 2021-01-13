abe4362f337914660dc8dc145faf03a4207a899b
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2021-01-13 17:22:52 +0530

    Update algorithms.py

--- pydatastructs/linear_data_structures/algorithms.py
+++ pydatastructs/linear_data_structures/algorithms.py
@@ -676,18 +676,18 @@ def quick_sort(array: Array, **kwargs) -> Array:
     def partition(low, high):
         i = (low - 1)
         x = array[high]
-        for j in range(l , high):
+        for j in range(low , high):
             if array[j] <= x:
                 i = i + 1
                 array[i], array[j] = array[j], array[i]
         array[i + 1], array[high] = array[high], array[i + 1]
         return (i + 1)
-    
+
     lower = kwargs.get('start', 0)
     upper = kwargs.get('end', len(array) - 1)
     comp = kwargs.get("comp", lambda u, v: u <= v)
     stack, top = [0] * (upper - lower + 1), -1
-    
+
     top += 1
     stack[top] = lower
     top += 1
