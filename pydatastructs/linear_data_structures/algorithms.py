4835dd42785bfab6603817be09708222aaa82533
Arvind Raj T <46020497+Arvind-raj06@users.noreply.github.com>
2021-01-13 16:45:24 +0530

    Adding quick sort

--- pydatastructs/linear_data_structures/algorithms.py
+++ pydatastructs/linear_data_structures/algorithms.py
@@ -673,3 +673,36 @@ def quick_sort(array: Array, **kwargs) -> Array:
 
     .. [1] https://en.wikipedia.org/wiki/Quicksort
     """
+    def partition(arr,l,h):
+        i = ( l - 1 )
+        x = arr[h]
+        for j in range(l , h):
+            if arr[j] <= x:
+                i = i+1
+                arr[i],arr[j] = arr[j],arr[i]
+        arr[i+1],arr[h] = arr[h],arr[i+1]
+        return (i+1)
+    
+    size = h - l + 1
+    stack = [0] * (size)
+    top = -1
+    top += 1
+    stack[top] = l
+    top += 1
+    stack[top] = h
+    while top >= 0:
+        h = stack[top]
+        top -= 1
+        l = stack[top]
+        top -= 1
+        p = partition( arr, l, h )
+        if p - 1 > l:
+            top += 1
+            stack[top] = l
+            top += 1
+            stack[top] = p - 1
+        if p + 1 < h:
+            top += 1
+            stack[top] = p + 1
+            top += 1
+            stack[top] = h
