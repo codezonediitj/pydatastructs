dfbaa8b0ff5be77f2470d908f3c9f4d519c3ee24
pratikgl <pratikgoyal2712@gmail.com>
2021-10-17 15:07:39 +0530

    Sparse table for RMQ added

---
+++ pydatastructs/miscellaneous_data_structures/algorithms.py
@@ -0,0 +1,32 @@
+from pydatastructs.miscellaneous_data_structures.sparse_table import SparseTable
+
+
+class RangeMinimumQuery:
+    def __new__(cls, array, ds='sparse_table'):
+        if ds == 'array':
+            pass
+        elif ds == 'sparse_table':
+            return RangeMinimumQuerySparseTable(cls)
+        else:
+            raise NotImplementedError(
+                "Currently %s data structure for range "
+                "minimum query isn't implemented yet."
+                % (ds))
+
+    def query(L, R):
+        raise NotImplementedError(
+            "This is an abstract method.")
+
+
+class RangeMinimumQuerySparseTable(RangeMinimumQuery):
+    
+    __slots__ = ["sparse_table"]
+
+    def __new__(cls, array):
+        obj = object.__new__(cls)
+        sparse_table = SparseTable(array)
+        obj.sparse_table = sparse_table
+        return obj
+
+    def query(self, i, j):
+        return self.sparse_table.__rangequery__(i, j)
