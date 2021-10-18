624635417fdcf40a00b0ab851c3320b26ada63c5
Gagandeep Singh <gdp.1807@gmail.com>
2021-10-18 14:09:27 +0530

    Update release to dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'PyDataStructs Development Team'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = 'dev'
 
 
 # -- General configuration ---------------------------------------------------
