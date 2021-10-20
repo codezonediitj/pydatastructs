62141e862587744c250d3c6e8d2a9e3ed7782ab5
pratikgl <pratikgoyal2712@gmail.com>
2021-10-20 13:24:41 +0530

    updated __new__

--- pydatastructs/miscellaneous_data_structures/sparse_table.py
+++ pydatastructs/miscellaneous_data_structures/sparse_table.py
@@ -21,35 +21,26 @@ class SparseTable(object):
     def __new__(cls, array, N=500000):
         obj = object.__new__(cls)
         MAXLOG = int(math.log2(2*N))
-
-    def __init__(self, array, N):
-        self.table = MultiDimensionalArray(int, 2*N+10, self.MAXLOG)
-        self.logs = OneDimensionalArray(int, 2*N+10)
-        self.logs[0] = self.logs[1] = 0
+        obj.table = MultiDimensionalArray(int, 2*N+10, MAXLOG)
+        obj.logs = OneDimensionalArray(int, 2*N+10)
+        obj.logs[0] = obj.logs[1] = 0
         for i in range(2, N):
-            self.logs[i] = self.logs[int(i/2)] + 1
+            obj.logs[i] = obj.logs[int(i/2)] + 1
+        for i in range(len(array)):
+            obj.table[i][0] = array[i]
+        for j in range(1, obj.MAXLOG):
+            for i in range(len(array) - (1 << j) + 1):
+                obj.table[i][j] = min(
+                    obj.table[i][j-1],
+                    obj.table[i+(1 << (j-1))][j-1])
+        return obj
 
     @classmethod
     def methods(cls):
-        return ['__createtable__', '__rangequery__']
+        return ['__rangequery__']
 
-    def _comp(x, y):
-        if(x < y):
-            return x
-        else:
-            return y
-
-    def __createtable__(self, arr, comp=_comp):
-        for i in range(len(arr)):
-            self.table[i][0] = arr[i]
-        for j in range(1, self.MAXLOG):
-            for i in range(len(arr) - (1 << j) + 1):
-                self.table[i][j] = comp(
-                    self.table[i][j-1],
-                    self.table[i+(1 << (j-1))][j-1])
-
-    def __rangequery__(self, L, R, comp=_comp):
-        j = self.logs[R-L+1]
-        return comp(
-            self.table[L][j],
-            self.table[R-(1 << j)+1][j])
+    def __rangequery__(self, left, right):
+        j = self.logs[right-left+1]
+        return min(
+            self.table[left][j],
+            self.table[right-(1 << j)+1][j])
