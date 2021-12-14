0fcf5e8e31399fa097ffae2e505284d6e83b13ae
Bhavesh <bjain4590@gmail.com>
2021-12-15 00:14:16 +0530

    Added BM algo

--- pydatastructs/strings/algorithms.py
+++ pydatastructs/strings/algorithms.py
@@ -158,3 +158,40 @@ def _rabin_karp(text, query):
             positions.append(i)
 
     return positions
+
+
+
+
+# Boyer-Moore Implementation for string pattern matching
+
+NO_OF_CHARS = 256
+
+def badCharHeuristic(string, size):
+
+	badChar = [-1]*NO_OF_CHARS
+
+	for i in range(size):
+		badChar[ord(string[i])] = i
+
+	return badChar
+
+def bmsearch(txt, pat):
+	
+	m = len(pat)
+	n = len(txt)
+
+	badChar = badCharHeuristic(pat, m)
+
+	s = 0
+	while(s <= n-m):
+		j = m-1
+
+		while j>=0 and pat[j] == txt[s+j]:
+			j -= 1
+
+		if j<0:
+			print("Pattern occur at shift = {}".format(s))
+			s += (m-badChar[ord(txt[s+m])] if s+m<n else 1)
+		else:
+			s += max(1, j-badChar[ord(txt[s+j])])
+
