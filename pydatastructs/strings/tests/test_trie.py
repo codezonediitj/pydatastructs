eede6270b71bccc2d207ab6b4cd9a546d91e5a3a
sak-codes <sakshioza17@gmail.com>
2023-03-16 14:10:27 +0530

    Add test.

--- pydatastructs/strings/tests/test_trie.py
+++ pydatastructs/strings/tests/test_trie.py
@@ -2,13 +2,20 @@ from pydatastructs import Trie
 
 def test_Trie():
 
-    strings = ["A", "to", "tea", "ted", "ten", "i", "in", "inn"]
+    strings = ["A", "to", "tea", "ted", "ten", "i",
+               "in", "inn", "Amfn", "snbr"]
     trie = Trie()
     for string in strings:
         trie.insert(string)
 
+    prefix_strings = ["te", "t", "Am", "snb"]
+
     for string in strings:
         assert trie.is_present(string)
+
+    for string in prefix_strings:
+        assert trie.is_prefix_present(string)
+        assert not trie.is_present(string)
 
     assert sorted(trie.strings_with_prefix("t")) == ['tea', 'ted', 'ten', 'to']
     assert sorted(trie.strings_with_prefix("te")) == ["tea", "ted", "ten"]
@@ -27,3 +34,4 @@ def test_Trie():
             else:
                 assert trie.is_present(present)
         strings.remove(string)
+     
