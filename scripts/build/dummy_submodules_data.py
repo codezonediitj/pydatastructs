47071eccf2bc805bab6fd1045354f7b1d49ffed3
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-05-07 15:00:31 +0530

    added _trees to dummy_submodules_data

--- scripts/build/dummy_submodules_data.py
+++ scripts/build/dummy_submodules_data.py
@@ -1,9 +1,9 @@
 project = 'pydatastructs'
 
-modules = ['linear_data_structures', 'miscellaneous_data_structures']
+modules = ['linear_data_structures', 'miscellaneous_data_structures', 'trees']
 
 backend = '_backend'
 
 cpp = 'cpp'
 
-dummy_submodules_list = [('_arrays.py', '_algorithms.py'), ('_stack.py',)]
+dummy_submodules_list = [('_arrays.py', '_algorithms.py'), ('_stack.py',), ('_trees.py')]
