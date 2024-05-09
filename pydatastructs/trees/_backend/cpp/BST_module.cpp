e3e57c7e9a47538bd8df91a12ec73c187e64776e
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-05-09 11:30:37 +0530

    removed whitespaces at line ends

--- pydatastructs/trees/_backend/cpp/BST_module.cpp
+++ pydatastructs/trees/_backend/cpp/BST_module.cpp
@@ -1,11 +1,3 @@
-//
-//  BST_module.c
-//  BST
-//
-//  Created by dwd on 12/27/17.
-//  Copyright © 2017 holdendou. All rights reserved.
-//
-
 #include "BST_module.hpp"
 
 /*
@@ -16,20 +8,20 @@ static void BSTDealloc(BST* self) {
         Py_DECREF(Py_None);
         return;
     }
-    
+
     // post-order traversal to dealloc descentents
     BSTDealloc((BST*)self->left);
     BSTDealloc((BST*)self->right);
-    
+
     Py_XDECREF(self->key);
     Py_XDECREF(self->data);
     Py_TYPE(self)->tp_free((PyObject*)self);
-    
+
 }
 
 static PyObject* BSTAlloc(PyTypeObject *type, PyObject *args, PyObject *kwds) {
     BST * self = (BST*)type->tp_alloc(type, 0);
-    
+
     // set all three as None
     if (self) {
         Py_INCREF(Py_None);
@@ -41,7 +33,7 @@ static PyObject* BSTAlloc(PyTypeObject *type, PyObject *args, PyObject *kwds) {
         self->left = Py_None;
         self->right = Py_None;
     }
-    
+
     return (PyObject*)self;
 }
 
@@ -49,27 +41,27 @@ static int BSTInit(BST* self, PyObject *args, PyObject *kwds) {
     char * kwlist[] = {"key","data", NULL};
     PyObject* key = NULL;
     PyObject* data = NULL;
-    
+
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO", kwlist, &key, &data)) {
         return -1;
     }
 
     if (key) {
-        Py_INCREF(key);        
+        Py_INCREF(key);
         PyObject* tmp = self->key;
         self->key = key;
         Py_XDECREF(tmp);
     }
 
     if (data) {
-        Py_INCREF(data);        
+        Py_INCREF(data);
         PyObject* tmp = self->data;
         self->data = data;
         Py_XDECREF(tmp);
     }
-    
+
     return 0;
-    
+
 }
 
 static PyTypeObject BSTType = {
@@ -148,11 +140,10 @@ static PyObject* BSTSearch(BST* self, PyObject* args) {
     return (PyObject*)self;
 }
 
-
 static PyObject* BSTInsert(BST* self, PyObject* args, PyObject* kwargs) {
     static char* keywords[] = {"key", "data", "comparator", NULL};
     PyObject *data = NULL, *key = NULL, *comparator = NULL;
-    
+
     if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO|O", keywords, &key, &data, &comparator)) {
         return NULL;
     }
@@ -163,8 +154,8 @@ static PyObject* BSTInsert(BST* self, PyObject* args, PyObject* kwargs) {
         BSTInit(b, argument, NULL);
         Py_DECREF(argument);
         return (PyObject*)b;
-    }    
-    
+    }
+
     if (comparator == NULL) {
         PyObject* tmp;
         if (self->key > key) { // curr key is larger; insert left
@@ -178,7 +169,7 @@ static PyObject* BSTInsert(BST* self, PyObject* args, PyObject* kwargs) {
             self->right = BSTInsert((BST*)self->right, args, kwargs);
             Py_DECREF(tmp);
         }
-        
+
         Py_INCREF(self);
         return (PyObject*)self;
     } else {
@@ -191,15 +182,15 @@ static PyObject* BSTInsert(BST* self, PyObject* args, PyObject* kwargs) {
         PyObject* arguments = Py_BuildValue("OO", self->key, key);
         PyObject* comp = PyObject_CallObject(comparator, arguments);
         Py_DECREF(arguments);
-        
+
         if (!PyLong_Check(comp)) {
             PyErr_SetString(PyExc_TypeError, "bad return type from comparator");
             return NULL;
         }
-        
+
         long long comp_result = PyLong_AsLongLong(comp);
         Py_DECREF(comp);
-        
+
         PyObject* tmp;
         if (comp_result == 1) { // curr key is larger; insert left
             tmp = self->left;
@@ -212,54 +203,11 @@ static PyObject* BSTInsert(BST* self, PyObject* args, PyObject* kwargs) {
             self->right = BSTInsert((BST*)self->right, args, kwargs);
             Py_DECREF(tmp);
         }
-        
+
         Py_INCREF(self);
         return (PyObject*)self;
     }
 }
-
-// The below insert() works completely without keywords
-
-// static PyObject* BSTInsert(BST* self, PyObject* args) {
-//     PyObject *key, *elem, *comparator = NULL;
-//     if (!PyArg_ParseTuple(args, "OOO", &key, &elem, &comparator)) {
-//         return NULL;
-//     }
-    
-//     if (!PyCallable_Check(comparator)) {
-//         PyErr_SetString(PyExc_ValueError, "comparator should be callable");
-//         return NULL;
-//     }
-//     if (self == Py_None) {
-//         BST * b = BSTAlloc(&BSTType, NULL, NULL);
-//         PyObject* argument = Py_BuildValue("(OO)", key, elem);
-//         BSTInit(b, argument, NULL);
-//         Py_DECREF(argument);
-//         return b;
-//     }
-//     PyObject* arguments = Py_BuildValue("OO", self->key, elem);
-//     PyObject* comp = PyObject_CallObject(comparator, arguments);
-//     Py_DECREF(arguments);
-//     if (!PyLong_Check(comp)) {
-//         PyErr_SetString(PyExc_TypeError, "bad return type from comparator");
-//         return NULL;
-//     }
-//     long long comp_result = PyLong_AsLongLong(comp);
-//     Py_DECREF(comp);
-//     PyObject* tmp;
-//     if (comp_result == 1) { // curr elem is larger; insert left
-//         tmp = self->left;
-//         self->left = BSTInsert(self->left, args);
-//         Py_DECREF(tmp);
-//     } else {
-//         tmp = self->right;
-//         self->right = BSTInsert(self->right, args);
-//         Py_DECREF(tmp);
-//     }
-//     Py_INCREF(self);
-//     return self;
-// }
-
 
 static PyModuleDef BSTmodule = {
     PyModuleDef_HEAD_INIT,
@@ -268,7 +216,6 @@ static PyModuleDef BSTmodule = {
     -1,
     NULL, NULL, NULL, NULL, NULL
 };
-
 
 PyMODINIT_FUNC PyInit_BST(void) {
     if (PyType_Ready(&BSTType) < 0) {
@@ -282,10 +229,3 @@ PyMODINIT_FUNC PyInit_BST(void) {
     PyModule_AddObject(m, "BST", (PyObject*)&BSTType);
     return m;
 }
-
-
-
-
-
-
-
