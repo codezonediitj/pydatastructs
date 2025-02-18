2b915d06adef324f7c4d73c54656e5c0b04e2771
30215210 <susmita.chakrabarty@ucalgary.ca>
2025-02-18 14:23:07 -0700

    Modify test_chacha20.py

--- pydatastructs/crypto/tests/test_chacha20.py
+++ pydatastructs/crypto/tests/test_chacha20.py
@@ -2,7 +2,7 @@ import random
 import string
 from pydatastructs.crypto.ChaCha20 import ChaCha20
 
-VALID_KEY = b"\x00" *32
+VALID_KEY = B"\x00" *32
 assert len(VALID_KEY) == 32, "VALID_KEY must be exactly 32 bytes"
 VALID_NONCE = B"\x00" * 12
 assert len(VALID_NONCE) == 12, "VALID_NONCE must be exactly 12 bytes"
