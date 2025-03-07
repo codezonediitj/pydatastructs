2207194bf961c2db448c1ff9ac10d2fdf3139d0c
30215210 <susmita.chakrabarty@ucalgary.ca>
2025-03-07 01:52:26 -0700

    Modify test_graph_snapshots.py

--- tests/test_graph_snapshots.py
+++ tests/test_graph_snapshots.py
@@ -5,19 +5,21 @@ from pydatastructs.graphs import Graph
 class TestGraphSnapshots(unittest.TestCase):
     def test_snapshot_creation(self):
         graph = Graph(implementation='adjacency_list')
-        graph.add_edge("A", "B", weight=5)
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_edge("A", "B", cost=5)
         graph.add_snapshot()
 
         self.assertEqual(len(graph.list_snapshots()), 1)
 
     def test_snapshot_retrieval(self):
         graph = Graph(implementation='adjacency_list')
-        graph.add_edge("A", "B", weight=5)
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_edge("A", "B", cost=5)
         graph.add_snapshot()
-        
         snapshot_time = graph.list_snapshots()[0]
         retrieved_graph = graph.get_snapshot(snapshot_time)
-
         self.assertEqual(retrieved_graph.is_adjacent("A", "B"), True)
 
     def test_invalid_snapshot(self):
@@ -27,3 +29,4 @@ class TestGraphSnapshots(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
