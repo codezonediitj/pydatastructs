baaa06da58b75f0d9a6b1b13b08fe7c4100f38a3
FamALouiz <fam@awadlouis.com>
2025-03-19 15:40:15 +0100

    Added parent pointer tree skeleton tests

--- pydatastructs/trees/tests/test_m_ary_trees.py
+++ pydatastructs/trees/tests/test_m_ary_trees.py
@@ -1,5 +1,9 @@
 from pydatastructs import MAryTree
+from pydatastructs.trees.m_ary_trees import ParentPointerTree
 
 def test_MAryTree():
     m = MAryTree(1, 1)
     assert str(m) == '[(1, 1)]'
+
+def test_ParentPointerTree(): 
+    pass
