923dbabe07030a2bdae9d5b86e69d105d50ee8df
pvprajwal <74557086+pvprajwal@users.noreply.github.com>
2025-04-02 20:42:22 +0530

    implemented ChaCha-20
    *implemented and added tests for ChaCha-20 Poly1305

--- pydatastructs/strings/algorithms.py
+++ pydatastructs/strings/algorithms.py
@@ -334,3 +334,101 @@ class Crypto:
             h = [(x + y) & 0xFFFFFFFF for x, y in zip(h, [a, b, c, d, e, f, g, h0])]
 
         return ''.join(f'{value:08x}' for value in h)
+
+    @staticmethod
+    def yield_chacha20_xor_stream(key, iv, position=0):
+        """Generate the xor stream with the ChaCha20 cipher."""
+        if not isinstance(position, int):
+            raise TypeError
+        if position & ~0xFFFFFFFF:
+            raise ValueError("Position is not uint32.")
+        if not isinstance(key, bytes):
+            raise TypeError
+        if not isinstance(iv, bytes):
+            raise TypeError
+        if len(key) != 32:
+            raise ValueError
+        if len(iv) != 8:
+            raise ValueError
+
+        def rotate(v, c):
+            return ((v << c) & 0xFFFFFFFF) | v >> (32 - c)
+
+        def quarter_round(x, a, b, c, d):
+            x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+            x[d] = rotate(x[d] ^ x[a], 16)
+            x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+            x[b] = rotate(x[b] ^ x[c], 12)
+            x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+            x[d] = rotate(x[d] ^ x[a], 8)
+            x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+            x[b] = rotate(x[b] ^ x[c], 7)
+
+        ctx = [0] * 16
+        ctx[:4] = (1634760805, 857760878, 2036477234, 1797285236)
+        ctx[4:12] = struct.unpack("<8L", key)
+        ctx[12] = ctx[13] = position
+        ctx[14:16] = struct.unpack("<LL", iv)
+        while 1:
+            x = list(ctx)
+            for i in range(10):
+                quarter_round(x, 0, 4, 8, 12)
+                quarter_round(x, 1, 5, 9, 13)
+                quarter_round(x, 2, 6, 10, 14)
+                quarter_round(x, 3, 7, 11, 15)
+                quarter_round(x, 0, 5, 10, 15)
+                quarter_round(x, 1, 6, 11, 12)
+                quarter_round(x, 2, 7, 8, 13)
+                quarter_round(x, 3, 4, 9, 14)
+            for c in struct.pack(
+                "<16L", *((x[i] + ctx[i]) & 0xFFFFFFFF for i in range(16))
+            ):
+                yield c
+            ctx[12] = (ctx[12] + 1) & 0xFFFFFFFF
+            if ctx[12] == 0:
+                ctx[13] = (ctx[13] + 1) & 0xFFFFFFFF
+
+    @staticmethod
+    def chacha20(data:bytes, key:bytes, nonce=None, position=0):
+        """
+        Encrypt (or decrypt) data using the ChaCha20 stream cipher.
+
+        Parameters
+        ==========
+        data : bytes
+            Input data to encrypt/decrypt
+        key : bytes
+            32-byte encryption key. Shorter keys will be padded by repetition.
+        nonce : bytes, optional
+            8-byte nonce
+
+        Returns
+        =======
+        bytes
+            Processed output
+
+        Notes
+        =====
+        Chacha20 is symmetric - same operation encrypts and decrypts.
+
+        References
+        ==========
+
+        .. [1] https://en.wikipedia.org/wiki/ChaCha20-Poly1305
+        .. [2] https://github.com/pts/chacha20/blob/master/chacha20_python3.py
+
+        """
+
+        if nonce is None:
+            nonce = b"\0" * 8
+
+        if not key:
+            raise ValueError("Key is empty.")
+        if len(key) < 32:
+            key = (key * (32 // len(key) + 1))[:32]
+        if len(key) > 32:
+            key = key[:32]
+
+        return bytes(
+            a ^ b for a, b in zip(data, Crypto.yield_chacha20_xor_stream(key, nonce, position))
+        )
