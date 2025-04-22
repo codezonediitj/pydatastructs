54146ac7fdaf0101a52a319baeb18f2d3ce963b7
30215210 <susmita.chakrabarty@ucalgary.ca>
2025-04-22 12:05:10 +0600

    Refactor: removed obsolete game code, updated ZKP tutorial scenes

--- ZKP_Demo_Tool/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from level_selector import LevelSelector
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = LevelSelector()
-    window.show()
-    sys.exit(app.exec_())
