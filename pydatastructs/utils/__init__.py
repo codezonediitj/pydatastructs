24b1394b27ed3f3e9775d3c0df30fa2b2e128604
Prerak Singh <prerak@Preraks-MacBook-Air.local>
2025-10-01 21:39:16 +0530

    bug fix

--- pydatastructs/utils/__init__.py
+++ pydatastructs/utils/__init__.py
@@ -5,14 +5,18 @@ from . import (
     testing_util,
 )
 
+from ._backend.cpp import _graph_utils
+
+AdjacencyListGraphNode = _graph_utils.AdjacencyListGraphNode
+AdjacencyMatrixGraphNode = _graph_utils.AdjacencyMatrixGraphNode
+GraphNode = _graph_utils.GraphNode
+GraphEdge = _graph_utils.GraphEdge
+
 from .misc_util import (
     TreeNode,
     MAryTreeNode,
     LinkedListNode,
     BinomialTreeNode,
-    AdjacencyListGraphNode,
-    AdjacencyMatrixGraphNode,
-    GraphEdge,
     Set,
     CartesianTreeNode,
     RedBlackTreeNode,
@@ -25,5 +29,11 @@ from .misc_util import (
 )
 from .testing_util import test
 
+__all__.extend([
+    'AdjacencyListGraphNode',
+    'AdjacencyMatrixGraphNode',
+    'GraphNode',
+    'GraphEdge',
+])
 __all__.extend(misc_util.__all__)
 __all__.extend(testing_util.__all__)
